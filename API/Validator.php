6b81354df02d6bc36e0e446fdb8a65b2062355a5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-03 01:25:44 +0300

    0.1.0

--- API/Validator.php
+++ API/Validator.php
@@ -21,15 +21,6 @@ final class Validator extends \Df\API\Response\Validator {
 	/**
 	 * 2017-07-06
 	 * @override
-	 * @see \Df\API\Exception::long()
-	 * @used-by \Df\API\Client::_p()
-	 * @return string
-	 */
-	function long() {return df_json_encode($this->r());}
-
-	/**
-	 * 2017-07-06
-	 * @override
 	 * @see \Df\API\Exception::short()
 	 * @used-by \Df\API\Client::_p()
 	 * @return string
