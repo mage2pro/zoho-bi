a8e25b030f140809716c8d1fa661996fb7a506d3
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-11 03:26:29 +0000

    https://github.com/mage2pro/core/issues/174

--- API/Facade.php
+++ API/Facade.php
@@ -37,13 +37,12 @@ abstract class Facade {
 
 	/**
 	 * 2017-07-06
-	 * @param string $ns [optional]
 	 * @param array(string => mixed) $p [optional]
 	 * @param string|null $method [optional]
 	 * @return array(string => mixed)
 	 * @throws DFE
 	 */
-	final function p(string $path, $ns = '', array $p = [], $method = null) {return C::i(
+	final function p(string $path, string $ns = '', array $p = [], $method = null) {return C::i(
 		$this, df_cc_path($ns, $path), $p, $method
 	)->p()[$path];}
 
